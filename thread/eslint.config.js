d6ec3a2f337607c65f5eab470a10c661464b879d
Marine Langrez <115779031+woorzz@users.noreply.github.com>
2025-05-11 20:15:44 +0200

    chore: add eslint thread and sender

---
+++ thread/eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: [".nuxt/**", "node_modules/**"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  },
+]);
