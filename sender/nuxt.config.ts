4c59614fd54b06cf857abf79424a53add01f588b
Marine Langrez <115779031+woorzz@users.noreply.github.com>
2025-05-12 21:21:57 +0200

    fix: tailwind with vite

--- sender/nuxt.config.ts
+++ sender/nuxt.config.ts
@@ -1,7 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBase: process.env.API_BASE || 'http://api:3000'
     }
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+
 })
